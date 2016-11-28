bcd585c2fc941234f44ff444c2334d12ca25a9bb
Luis Felipe Cortes <lfe.cortes@gmail.com>
2016-11-28 17:39:59 -0200

    create basic validations booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :republica
+
+  validates :check_in, presence: true
+  validates :check_out, presence: true
+  validates :status, presence: true
 end
