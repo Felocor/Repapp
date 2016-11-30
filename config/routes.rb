7a30fdb8e1d3c9ac83fc2922b8acff69cc321545
Rodrigo Birolli <rodrigo.birolli@gmail.com>
2016-11-30 15:09:24 -0200

    User's booking list and edit implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
   resources :republicas do
     resources :bookings
   end
+
+  get '/user/:id/list_users', to: 'bookings#list_users', as: 'user_booking_list'
+
 end
