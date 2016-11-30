12845c23449d23987560ac3ec8750de099e4a2b6
Antoine Moreau <antoine_moreau@me.com>
2016-11-30 11:51:36 -0200

    Finish homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "pages#home"
   resources :republicas do
     resources :bookings
   end
