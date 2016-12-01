b73caf10ee9f52fe55751d24c113e6dd2c14afb4
Antoine Moreau <antoine_moreau@me.com>
2016-12-01 11:44:53 -0200

    Update homepage front

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  # Root path
   root to: "pages#home"
-  get '/user/:id/list_users', to: 'bookings#list_users', as: 'user_booking_list'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  # User's bookings path
+  get '/user/:id/list_users', to: 'bookings#list_users', as: 'user_booking_list'
+
+  # FaceBook connect callback
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  # Resources path
   resources :republicas do
     resources :bookings
   end
