d264233830848bdc76974acc1bfbd4349b3fd2f9
Rodrigo Birolli <rodrigo.birolli@gmail.com>
2016-12-01 15:14:58 -0200

    Implemented Republica's booking approving/rejecting functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   get '/user/:id/list_users', to: 'bookings#list_users', as: 'user_booking_list'
+  get '/republica/:id/booking/:id/confirm_booking', to: 'bookings#confirm_booking', as: 'confirm_booking'
+  get '/republica/:id/booking/:id/reject_booking', to: 'bookings#reject_booking', as: 'reject_booking'
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :republicas do
